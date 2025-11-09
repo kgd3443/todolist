0ba55304f4d94d99c8c310244d54cd7c6cd22781
kgd3443 <shyshy7665@naver.com>
2025-11-09 16:28:32 +0900

    123

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If building on GitHub Actions, infer repo name for `base` automatically.
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
-
 export default defineConfig({
-  base: repoName ? `/${repoName}/` : '/',
+  base: '/todolist/',
   plugins: [react()],
 })
